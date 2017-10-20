b08fd7eee0d87fc7817e42c50f2adf92f59f21e0
Raynhour <waqdet@gmail.com>
2017-10-20 16:11:47 +0300

    first

---
+++ js/main.js
@@ -0,0 +1,96 @@
+$(document).ready(function(){
+
+      // hide our element on page load
+  $('#list_1').css('opacity', 0);
+  $("#list_2").css("opacity",0);
+  $("#list_3").css("opacity",0);
+  $("#list_4").css("opacity",0);
+
+  $('#Openbit_anchors').waypoint(function(direction) {
+    if (direction === 'down') {
+      // reveal our content
+      $('#list_1').addClass('fadeInUp');
+      $('#list_1').removeClass('fadeOutDown');
+    } else if (direction === 'up') {
+      // hide our content
+      $('#list_1').addClass('fadeOutDown');
+      $('#list_1').removeClass('fadeInUp');
+    }
+ 
+  }, { offset: '100%' });
+  $('#list_2').waypoint(function(direction) {
+    if (direction === 'down') {
+      // reveal our content
+      $('#list_2').addClass('fadeInUp');
+      $('#list_2').removeClass('fadeOutDown');
+    } else if (direction === 'up') {
+      // hide our content
+      $('#list_2').addClass('fadeOutDown');
+      $('#list_2').removeClass('fadeInUp');
+    }
+ 
+  }, { offset: '100%' });
+   $('#list_3').waypoint(function(direction) {
+    if (direction === 'down') {
+      // reveal our content
+      $('#list_3').addClass('fadeInUp');
+      $('#list_3').removeClass('fadeOutDown');
+    } else if (direction === 'up') {
+      // hide our content
+      $('#list_3').addClass('fadeOutDown');
+      $('#list_3').removeClass('fadeInUp');
+    }
+ 
+  }, { offset: '100%' });
+
+ 
+  $('#modificator').waypoint(function(direction) {
+    if (direction === 'down') {
+      // reveal our content
+      $('#list_4').addClass('fadeInUp');
+      $('#list_4').removeClass('fadeOutDown');
+    } else if (direction === 'up') {
+      // hide our content
+      $('#list_4').addClass('fadeOutDown');
+      $('#list_4').removeClass('fadeInUp');
+    } 
+  }, { offset: '100%' }); 
+  $('#shield').waypoint(function(direction) {
+    if (direction === 'down') {
+      // reveal our content
+      $('#shield').addClass('zoomIn');
+      $('#shield').removeClass('zoomOut');
+    } else if (direction === 'up') {
+      // hide our content
+      $('#shield').addClass('zoomOut');
+      $('#shield').removeClass('zoomIn');
+    } 
+  }, { offset: '100%' }); 
+  $('#shield').waypoint(function(direction) {
+    if (direction === 'down') {
+      // reveal our content
+      $('#shield').addClass('zoomIn');
+      $('#shield').removeClass('zoomOut');
+    } else if (direction === 'up') {
+      // hide our content
+      $('#shield').addClass('zoomOut');
+      $('#shield').removeClass('zoomIn');
+    } 
+  }, { offset: '100%' }); 
+
+  $('#blocks').waypoint(function(direction) {
+    if (direction === 'down') {
+      // reveal our content
+      $('.s6__left-1').css('transform', 'translateX('+ 0 +'%)');
+      $('.s6__right-1').css('transform', 'translateX('+ 0 +'%)');
+    } else if (direction === 'up') {
+      // hide our content
+      $('.s6__left-1').css('transform', 'translateX('+ 150 +'%)');
+      $('.s6__right-1').css('transform', 'translateX('+ -150 +'%)');
+    } 
+  }, { offset: '50%' }); 
+});
+
+$(".header-mobile").click(function(){
+    $(".header-nav").toggle();
+});
